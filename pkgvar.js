e2ecd2d47791863522b8e5d14f17870e6a198d64
Stijn de Witt <StijnDeWitt@hotmail.com>
2016-01-03 11:51:38 +0100

    0.1.0

---
+++ pkgvar.js
@@ -0,0 +1,16 @@
+﻿var
+fs = require('fs'),
+pkg = JSON.parse(fs.readFileSync('package.json')),
+objectPath = require('object-path');
+
+function pkgvar(path) {
+	var root = path ? objectPath.get(pkg, path) : pkg;
+	resolved = resolve(root);
+	return resolved;
+}
+
+function resolve(root, node, resolveTree) {
+	return root;
+}
+
+module.exports = pkgvar;
---
+++ pkgvar.spec.js
@@ -0,0 +1,38 @@
+﻿var pkgvar = require('../pkgvar');
+var expect = require('chai').expect;
+
+describe('pkgvar', function(){
+	it('is a function', function(){
+		expect(pkgvar).to.be.a('function');
+	});
+	it('returns an object', function(){
+		expect(pkgvar()).to.be.an('object');
+	});
+	it('accepts a string or array of strings as parameter', function(){
+		expect(pkgvar('test')).to.be.an('object');
+		expect(pkgvar(['test'])).to.be.an('object');
+	});
+	it('returns an object representation of package.json when given no arguments', function(){
+		var pkg = pkgvar();
+		expect(pkg).to.have.a.property('name');
+		expect(pkg.name).to.equal('pkgvar');
+		expect(pkg).to.have.a.property('version');
+		expect(pkg).to.have.a.property('description');
+		expect(pkg).to.have.a.property('test');
+		expect(pkg.test).to.have.a.property('data');
+		expect(pkg.test.data).to.have.a.property('array');
+		expect(pkg.test.data.array).to.be.an('array');
+	});
+	it('returns a subtree from package.json when given an argument', function(){
+		var test = pkgvar('test');
+		expect(test).to.have.a.property('data');
+		expect(test.data).to.have.a.property('array');
+		expect(test.data.array).to.be.an('array');
+	});
+/*
+	it('resolves references of the form `@pkg.object.path`', function(){
+		var pkg = pkgvar();
+		expect(pkg.test.primitive).to.equal(pkg.name);
+	});
+*/
+});
